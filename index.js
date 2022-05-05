22606c8178037dd71e3600c4ab1c6f970883a3b0
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-05-05 22:13:22 +0000

    funny and safe-for-work message

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const input = process.argv[2];
 const fs = require('fs');
 const data = require('./data.json');
 
+console.log(data)
+
 // fs.writeFile('index.js', input, (err) => {
 
 // fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
 if (input === 'read') {
-  console.log(data.notes);
-}
-
-for (const property in data.notes) {
-  console.log(`${property}: ${data.notes[property]}`);
+  for (const property in data.notes) {
+    console.log(`${property}: ${data.notes[property]}`);
+  }
+} else if (input === "create") {
+  console.log(data.nextId, data.notes)
+  data.notes[data.nextId] = process.argv[3]
+  data.nextId++;
+  console.log(data.nextId, data.notes)
 }
