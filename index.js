786a428fa7ffcd9e8c6344a74740665c8ccdd821
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-05-05 22:13:49 +0000

    funny and safe-for-work message

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ console.log(data)
 // fs.writeFile('index.js', input, (err) => {
 
 // fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
+
+
 if (input === 'read') {
   for (const property in data.notes) {
     console.log(`${property}: ${data.notes[property]}`);
@@ -14,6 +16,8 @@ if (input === 'read') {
 } else if (input === "create") {
   console.log(data.nextId, data.notes)
   data.notes[data.nextId] = process.argv[3]
-  data.nextId++;
+  data.nextId = data.nextId++;
   console.log(data.nextId, data.notes)
 }
+
+data.nextId =
