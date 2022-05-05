a4b19c73125e0cd87452013b0790e7d6cf9d216a
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-05 20:28:58 +0000

    node-notes-cli -LC

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const input = process.argv[2];
 const fs = require('fs');
-console.log(input);
+const data = require('./data.json');
 
-// fs.writeFile('index.js', third, (err) => {
-//   if (err) throw err;
-//   console.log('The file has been saved!');
-// });
+console.log(data);
+
+fs.writeFile('index.js', third, (err) => {
+  if (err) throw err;
+  console.log(third);
+});
 
 // fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
