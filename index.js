7a3e3531d2d00a22eff5c2ed4758dc60d11d79b1
Anna Park <parkannuh@gmail.com>
2022-05-05 20:17:12 +0000

    In progress.

---
+++ index.js
@@ -0,0 +1,10 @@
+const input = process.argv[2];
+const fs = require('fs');
+console.log(input);
+
+// fs.writeFile('index.js', third, (err) => {
+//   if (err) throw err;
+//   console.log('The file has been saved!');
+// });
+
+// fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
