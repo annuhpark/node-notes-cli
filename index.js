2beca4191d55ccaaffc5bf8193c2508b6f125d44
Lance Cruzado <raphaellcruzado@gmail.com>
2022-05-05 22:54:45 +0000

    node-notes-cli -LC

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const data = require('./data.json');
 
 // fs.writeFile('index.js', input, (err) => {
 
-// fs.writeFile('message.txt', 'Hello Node.js', 'utf8', callback);
-
-
 if (input === 'read') {
   for (const property in data.notes) {
     console.log(`${property}: ${data.notes[property]}`);
+  }}
+  } else if (input === 'create') {
+    data.notes[data.nextId] = process.argv[3];
+    data.nextId++;
+    console.log(data.nextId, data.notes);
+    fs.writeFile(data, JSON.stringify(data, null, 2), err => {
+      if (err) {
+        throw err
+      }
+    }
   }
-} else if (input === 'create') {
-  // console.log(data.nextId, data.notes)
-  data.notes[data.nextId] = process.argv[3]
-  data.nextId++;
-  console.log(data.nextId, data.notes)
-  // data.nextId++;
-  // data.notes.nextId = process.argv[3];
 }
